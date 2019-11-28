a009ca97fa1f1bc1c85c021b5d5ce538061c7fb8
Hoang Hiep <thanhdaovotinh2611@gmail.com>
2019-11-28 17:48:46 +0700

    implement firebase, google implement glide, get image from storage

--- app/src/main/java/com/example/apartmentcitizen/login/SplashActivity.java
+++ app/src/main/java/com/example/apartmentcitizen/login/SplashActivity.java
@@ -13,14 +13,14 @@ import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_TIME = 3000;
+    private final int SPLASH_TIME = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EasySplashScreen config = new EasySplashScreen(SplashActivity.this)
                 .withFullScreen()
-                .withTargetActivity(HomeActivity.class)
+                .withTargetActivity(LoginActivity.class)
                 .withSplashTimeOut(SPLASH_TIME)
                 .withLogo(R.drawable.logoapartment)
                 .withBackgroundColor(Color.parseColor("#FFFFFF"));
