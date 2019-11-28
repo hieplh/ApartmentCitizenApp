64525111cfefc0e2b396597312a9a92fac6891a6
Pham Duc Hieu <duchieupham.97@icloud.com>
2019-11-28 22:31:39 +0700

    continue HomeActivity
    .....

--- app/src/main/java/com/example/apartmentcitizen/home/HomeActivity.java
+++ app/src/main/java/com/example/apartmentcitizen/home/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.apartmentcitizen.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.apartmentcitizen.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -10,7 +14,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-
+    BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+    TextView lbTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,31 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void setupView(){
+        lbTitle = findViewById(R.id.lbTitle);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.navNewsfeed);
+        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()){
+                    case R.id.navSurvey:
+                        lbTitle.setText("Khảo sát");
+                        return true;
+                    case R.id.navNewsfeed:
+                        lbTitle.setText("Bảng tin");
+                        return true;
+                    case R.id.navDBoard:
+                        lbTitle.setText("Bảng điều khiển");
+                        return true;
+                    case R.id.navNoti:
+                        lbTitle.setText("Thông báo");
+                        return true;
+                }
+                return false;
+            }
+        };
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
+
