950ee9d1fe4fbfe1d40ba8422d5b0d4517596260
Pham Duc Hieu <duchieupham.97@icloud.com>
2019-12-10 11:15:03 +0700

    Add sth

---
+++ app/src/main/java/com/example/apartmentcitizen/home/dashboard/newsfeed/NewsfeedActivity.java
@@ -0,0 +1,16 @@
+package com.example.apartmentcitizen.home.dashboard.newsfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apartmentcitizen.R;
+
+public class NewsfeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newsfeed);
+    }
+}
